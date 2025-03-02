7f4af0583e2c61db421061282711b249daf1fc44
HocPhamIT <89366106+HocPhamThai@users.noreply.github.com>
2025-03-02 22:24:16 +0700

    Solved Contain Duplicate LeetCode

---
+++ HashMapAndSet/ContainsDuplicate.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashMapAndSet
+{
+    public static class ContainsDuplicate
+    {
+        public static bool ContainsDuplicate_Resolve_1(int[] nums)
+        {
+            // [1, 2, 3, 1] => true
+            // [1, 2, 3, 4] => false
+            // [1, 1, 1, 3, 3, 2, 4, 3] => true
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool ContainsDuplicate_Resolve_2(int[] nums)
+        {
+            int[] numsCopy = new int[nums.Length];
+            Array.Copy(nums, numsCopy, nums.Length);
+            Array.Sort(numsCopy);
+            for (int i = 1; i < numsCopy.Length; i++)
+            {
+                if (numsCopy[i] == numsCopy[i - 1])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool ContainsDuplicate_Resolve_3(int[] nums)
+        {
+            HashSet<int> hashSetNumber = new HashSet<int>();
+
+            foreach (var num in nums)
+            {
+                if (!hashSetNumber.Contains(num))
+                {
+                    hashSetNumber.Add(num);
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- HashMapAndSet/Program.cs
+++ HashMapAndSet/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace HashMapAndSet
+﻿
+namespace HashMapAndSet
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            // Contains Duplicate
+            TestContainDuplicate();
+        }
+
+        private static void TestContainDuplicate()
+        {
+            int[] nums1 = [1, 2, 3, 1]; // => true
+            int[] nums2 = [1, 2, 3, 4];// => false
+            int[] nums3 = [1, 1, 1, 3, 3, 2, 4, 3]; // return true
+
+            var result = ContainsDuplicate.ContainsDuplicate_Resolve_1(nums1);
+            Console.WriteLine(result);
+            result = ContainsDuplicate.ContainsDuplicate_Resolve_1(nums2);
+            Console.WriteLine(result);
+            result = ContainsDuplicate.ContainsDuplicate_Resolve_1(nums3);
+            Console.WriteLine(result);
+
+            result = ContainsDuplicate.ContainsDuplicate_Resolve_2(nums1);
+            Console.WriteLine(result);
+            result = ContainsDuplicate.ContainsDuplicate_Resolve_2(nums2);
+            Console.WriteLine(result);
+            result = ContainsDuplicate.ContainsDuplicate_Resolve_2(nums3);
+            Console.WriteLine(result);
+
+            result = ContainsDuplicate.ContainsDuplicate_Resolve_3(nums1);
+            Console.WriteLine(result);
+            result = ContainsDuplicate.ContainsDuplicate_Resolve_3(nums2);
+            Console.WriteLine(result);
+            result = ContainsDuplicate.ContainsDuplicate_Resolve_3(nums3);
+            Console.WriteLine(result);
         }
     }
 }
